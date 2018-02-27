3b11b24c349c21a361476fa207171afec40e69ff
marco261371 <marco1475963@gmail.com>
2018-02-28 00:33:59 +0100

    Add coverage and test for initSuggestionObserver. #4

--- app/src/test/java/Assignment4/GroupedTests.java
+++ app/src/test/java/Assignment4/GroupedTests.java
@@ -1,22 +1,73 @@
 package Assignment4;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.os.Looper;
+
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.junit.Test;
+import org.schabi.newpipe.fragments.list.search.SearchFragment;
+import static junit.framework.Assert.*;
 
 /**
  * Created by emillindblom on 2018-02-25.
  */
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Log.class, Bundle.class, TextUtils.class, Looper.class})
 public class GroupedTests {
 
     @Test
-    public void exampleMethodTest() throws Exception {
-        //String methodName = "exampleMethod";
-        //System.out.println("Calculating branch coverage for " + methodName);
-        //CodeCoverage cc = new CodeCoverage(methodName);
+    public void initSuggestionObserverTest() throws Exception {
+        PowerMockito.mockStatic(Log.class);
+        PowerMockito.mockStatic(TextUtils.class);
+        PowerMockito.mockStatic(Looper.class);
+        Bundle mock = PowerMockito.mock(Bundle.class);
+        PowerMockito.whenNew(Bundle.class).withNoArguments().thenReturn(mock);
 
-        //assertEquals(The result, exampleMethod(some, data, that, visits, specific, branches, cc));
-        //assertEquals(The result, exampleMethod(some, data, that, visits, other, branches, cc);
-        //System.out.println(cc.toString());
+        String methodName = "initSuggestionObserver";
+        System.out.println("Calculating branch coverage for " + methodName);
+        CodeCoverage cc = new CodeCoverage(methodName);
+        Bundle outState = new Bundle();
+
+        // visit branch 0
+        SearchFragment sf = SearchFragment.getInstance(0,"");
+        sf.initSuggestionObserver(cc);
+
+        sf = SearchFragment.getInstance(1,"a");
+        sf.initSuggestionObserver(cc);
+       
+        System.out.println(cc.toString());
+    }
+
+    @Test
+    public void initSearchListenersTest() throws Exception {
+        PowerMockito.mockStatic(Log.class);
+        PowerMockito.mockStatic(TextUtils.class);
+        PowerMockito.mockStatic(Looper.class);
+        Bundle mock = PowerMockito.mock(Bundle.class);
+        PowerMockito.whenNew(Bundle.class).withNoArguments().thenReturn(mock);
+
+        String methodName = "initSearchListeners";
+        System.out.println("Calculating branch coverage for " + methodName);
+        CodeCoverage cc = new CodeCoverage(methodName);
+        //Bundle outState = new Bundle();
+
+        //visit branch 0
+        //SearchFragment sf = SearchFragment.getInstance(0,"");
+        //sf.initSearchListeners(cc);
+
+        
+        System.out.println(cc.toString());
     }
 
 }
