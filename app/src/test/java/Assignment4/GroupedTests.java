a7c73acfe123bac5e10f0a6d01d4f84ebfab52c5
Emil Lindblom <emillindblom95@gmail.com>
2018-02-28 11:38:58 +0100

    Fix errors

--- app/src/test/java/Assignment4/GroupedTests.java
+++ app/src/test/java/Assignment4/GroupedTests.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import Assignment4.CodeCoverage;
+
 
 import java.io.File;
 import java.util.LinkedList;
